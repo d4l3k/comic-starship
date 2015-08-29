800c937a39662310e01db8ba65a3663ffacbfdd9
Tristan Rice <rice@fn.lc>
2015-08-29 06:59:50 +0000

    Added loading spinner

--- build.go
+++ build.go
@@ -8,7 +8,7 @@ import (
 func vulcanize() ([]byte, error) {
 	log.Println("Vulcanizing...")
 	cmd := exec.Command("node_modules/vulcanize/bin/vulcanize", "--inline-scripts", "--inline-css", "public/app.html")
-	stdout, err := cmd.Output()
+	stdout, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Println("STDOUT ", string(stdout))
 		return nil, err
